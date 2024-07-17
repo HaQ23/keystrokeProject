c7d8627d7a20cda291f586b27458b5e980eae4ba
Krzysztof Dobosz <dobosz.krzysiek7@gmail.com>
2024-07-17 18:54:47 +0200

    add dockerfile

--- keystrokeApi/app/__init__.py
+++ keystrokeApi/app/__init__.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
 import logging
 
 def create_app():
     app = FastAPI()
+
+    uri = "mongodb+srv://krzysztofdobosz:keystroke0987654321@cluster0.6izhsw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     logging.basicConfig(level=logging.DEBUG)
 
     try:
-        client = MongoClient('mongodb://localhost:27017/')
+        client = MongoClient(uri, server_api=ServerApi('1'))
         app.db = client.keystrokeDB
         logging.info("Connected to MongoDB successfully.")
     except Exception as e:
         logging.error(f"Could not connect to MongoDB: {e}")
+
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
 
     from .controllers import router as controllers_router
     app.include_router(controllers_router)
